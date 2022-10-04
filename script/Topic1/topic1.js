5073648c824d0b8c624c30e7f2e784fa986b0dfe
ahsanv101 <33668504+ahsanv101@users.noreply.github.com>
2022-10-04 15:02:10 +0200

    sdsd

--- script/Topic1/topic1.js
+++ script/Topic1/topic1.js
@@ -6,8 +6,8 @@ String.prototype.tpl = function(o) {
     return r 
 }
 
-var listItemTpl = `<li><a href='#' onclick='load("$url")'>$label</a></li>`;
-var listItemTpl2 = `<li><a href='#' onclick='load2("$url")'>$label</a></li>`;
+var listItemTpl = `<li class="nav-item"><a href='#' onclick='load("$url")'>$label</a></li>`;
+var listItemTpl2 = `<li class="nav-item"><a href='#' onclick='load2("$url")'>$label</a></li>`;
 
 $(document).ready(main);
 
