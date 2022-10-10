f7e4780853dcb721dca29c8f2c0888614564fdfe
ahsanv101 <33668504+ahsanv101@users.noreply.github.com>
2022-10-10 17:15:58 +0200

    zczx

--- script/home/home.js
+++ script/home/home.js
@@ -2,20 +2,20 @@
 //           strings = ["Type", "Time"],
 //           currentWord = 1;
           
-window.setInterval(() => {
-    currentWord++;
-    if(currentWord >= strings.length) {
-        currentWord=0
-        strDynamicTitle.textContent = strings[currentWord];
-        strDynamicTitle.style.color = "unset";
-        strDynamicTitle.style.textShadow= "unset";
-    } else {
-        strDynamicTitle.textContent = strings[currentWord];
-        strDynamicTitle.style.color = "var(--time-color)";
-        strDynamicTitle.style.transition = "var(--time-transition)";
-        strDynamicTitle.style.textShadow = "var(--time-shadow)";
-    }
-}, 2000);
+// window.setInterval(() => {
+//     currentWord++;
+//     if(currentWord >= strings.length) {
+//         currentWord=0
+//         strDynamicTitle.textContent = strings[currentWord];
+//         strDynamicTitle.style.color = "unset";
+//         strDynamicTitle.style.textShadow= "unset";
+//     } else {
+//         strDynamicTitle.textContent = strings[currentWord];
+//         strDynamicTitle.style.color = "var(--time-color)";
+//         strDynamicTitle.style.transition = "var(--time-transition)";
+//         strDynamicTitle.style.textShadow = "var(--time-shadow)";
+//     }
+// }, 2000);
 
 const navbar = document.querySelector('.nav-fixed');
 const collapseNav = document.querySelector(".navbar-collapse");
