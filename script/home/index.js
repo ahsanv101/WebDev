a196bd63c2eb9b84090f98bce8be1badcbca703d
ahsanv101 <33668504+ahsanv101@users.noreply.github.com>
2022-10-27 16:54:12 +0200

    few changes in html

--- script/home/index.js
+++ script/home/index.js
@@ -1,97 +1,180 @@
 
-  
-  function changeThemeHome(id) {
-      const theme = document.getElementsByTagName('link')[1];
-      switch (id) {
-        case "plag":
-            console.log(theme);
-            theme.href = "css/blackplague/blackplague.css";
-            localStorage.setItem('selectedTheme', 'plag');
-            break;
-        case "colo":
-            theme.href = "css/colonization/colonization.css";
-            localStorage.setItem('selectedTheme', 'colo');
-            break;
-        case "crash":
-            theme.href = "css/wallstreet_crash/wallstreet_crash.css";
-            localStorage.setItem('selectedTheme', 'crash');
-            break;
-        case "cold":
-            theme.href = "css/thecoldwar/thecoldwar.css";
-            localStorage.setItem('selectedTheme', 'cold');
-            break;
-        case "cons":
-  
-            theme.href = "css/conspiracy/conspiracy.css";
-            localStorage.setItem('selectedTheme', 'cons');
-            break;
-        case "ali":
-            theme.href = "css/alieninvasion/alieninvasion.css";
-            localStorage.setItem('selectedTheme', 'ali');
-            break;
-        case 'default':
-        default:
-            theme.href = "css/colonization/colonization.css";
-            localStorage.setItem('selectedTheme', 'default');
-            break;
-      }
-    }
-    
-let docname = document.getElementsByTagName("title")[0];
-// console.log(docname.innerHTML);
-if(docname.innerHTML == 'Home'){
-
-
-    if (localStorage.getItem('selectedTheme')) {
-        // console.log('this is home block');
-        changeThemeHome(localStorage.getItem('selectedTheme'));
-        }
-
-}
-else{
-    if (localStorage.getItem('selectedTheme')) {
-        // console.log('else block not home');
-        changeThemeDoc(localStorage.getItem('selectedTheme'));
-        }
-}
-
-    
-  
-function changeThemeDoc(id) {
+function changeThemeHome(id) {
     const theme = document.getElementsByTagName('link')[1];
     switch (id) {
     case "plag":
-        theme.href = "../../css/blackplague/blackplague.css";
+        // console.log(theme);
+        theme.href = "css/blackplague/blackplague.css";
         localStorage.setItem('selectedTheme', 'plag');
         break;
+
     case "colo":
-        theme.href = "../../css/colonization/colonization.css";
+        theme.href = "css/colonization/colonization.css";
         localStorage.setItem('selectedTheme', 'colo');
         break;
+
     case "crash":
-        theme.href = "../../css/wallstreet_crash/wallstreet_crash.css";
+
+        theme.href = "css/wallstreet_crash/wallstreet_crash.css";
         localStorage.setItem('selectedTheme', 'crash');
         break;
+    
     case "cold":
-        theme.href = "../../css/thecoldwar/thecoldwar.css";
+        theme.href = "css/thecoldwar/thecoldwar.css";
         localStorage.setItem('selectedTheme', 'cold');
         break;
-    case "cons":
 
-        theme.href = "../../css/conspiracy/conspiracy.css";
+    case "cons":
+        theme.href = "css/conspiracy/conspiracy.css";
         localStorage.setItem('selectedTheme', 'cons');
         break;
+    
     case "ali":
-
-        theme.href = "../../css/alieninvasion/alieninvasion.css";
+        theme.href = "css/alieninvasion/alieninvasion.css";
         localStorage.setItem('selectedTheme', 'ali');
         break;
-    default:
-        theme.href = "../../css/colonization/colonization.css"; 
+        
+
+    case "default":
+        default:
+        theme.href = "css/colonization/colonization.css";
         localStorage.setItem('selectedTheme', 'default');
         break;
+
+    
+
     }
 }
+    
+
+function changeThemeDoc(id,name) {
+    const theme = document.getElementsByTagName('link')[1];
+
+    console.log(id,name);
+    switch (id) {
+
+    case "plag":
+        if (name == "Home"){ theme.href = "css/blackplague/blackplague.css";}
+        
+        else{
+            theme.href = "../../css/blackplague/blackplague.css";
+        }
+        localStorage.setItem('selectedTheme', 'plag');
+        break;
+       
+
+    case "colo":
+        if (name == "Home"){theme.href = "css/colonization/colonization.css";}
+        
+        else{
+            theme.href = "../../css/colonization/colonization.css";
+        }
+        
+        localStorage.setItem('selectedTheme', 'colo');
+        break;
+       
+
+    case "crash":
+        if (name == "Home"){ theme.href = "css/wallstreet_crash/wallstreet_crash.css";}
+        
+        else{
+            theme.href = "../../css/wallstreet_crash/wallstreet_crash.css";
+        }
+        
+        localStorage.setItem('selectedTheme', 'crash');
+        break;
+       
+
+    case "cold":
+        if (name == "Home"){theme.href = "css/thecoldwar/thecoldwar.css";}
+        
+        else{
+            theme.href = "../../css/thecoldwar/thecoldwar.css";
+        }
+        
+        localStorage.setItem('selectedTheme', 'cold');
+        break;
+        
+
+    case "cons":
+        if (name == "Home"){theme.href = "css/conspiracy/conspiracy.css";}
+        
+        else{
+            theme.href = "../../css/conspiracy/conspiracy.css";
+        }
+
+        
+        localStorage.setItem('selectedTheme', 'cons');
+        break;
+      
+
+    case "ali":
+        if (name == "Home"){theme.href = "css/alieninvasion/alieninvasion.css";}
+        
+        else{
+            theme.href = "../../css/alieninvasion/alieninvasion.css";
+        }
+
+        
+        localStorage.setItem('selectedTheme', 'ali');
+        break;
+       
+    
+    case "default":
+        default:
+        if (name == "Home"){theme.href = "css/colonization/colonization.css";}
+        
+        else{
+            theme.href = "../../css/colonization/colonization.css";
+        }
+
+        
+        localStorage.setItem('selectedTheme', 'default');
+        break;
+
+        
+
+         
+    }
+}
+
+
+docname = document.getElementsByTagName("title")[0];
+// console.log(localStorage);
+
+
+if(localStorage.getItem('selectedTheme')){
+
+
+    changeThemeDoc(localStorage.getItem('selectedTheme'),docname.innerHTML)
+    // docname.innerHTML == 'Home'
+
+    // console.log(localStorage);
+
+    // changeThemeHome("default");
+    // console.log(localStorage,'if');
+
+
+    // if (localStorage.getItem('selectedTheme')) {
+       
+    //     changeThemeHome(localStorage.getItem('selectedTheme'));
+    //     }
+}
+// else{
+    // console.log(localStorage,'else');
+    // changeThemeDoc("default");
+
+    // if (localStorage.getItem('selectedTheme')) {
+    //     changeThemeDoc(localStorage.getItem('selectedTheme'));
+    //     }
+
+// }
+// localStorage.clear(); 
+
+
+    
+  
+
 
   const navbar = document.querySelector('.nav-fixed');
   const collapseNav = document.querySelector(".navbar-collapse");
